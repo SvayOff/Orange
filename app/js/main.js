3ee75268ac10cdd3aa99bf42bd4faba42bd72a52
SvayOff <marsiul2001@gmail.com>
2022-05-09 23:56:35 +0300

    added slider page

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,9 @@
 $(function () {
+  $('.slider__inner').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<button type = "button" class= "slick-next" ><span class="icon-chevron-right"> </span> </button>',
+    prevArrow: '<button type = "button" class= "slick-prev" ><span class="icon-chevron-left"> </span> </button>',
+  });
 });
