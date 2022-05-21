2fddf94c124782d4ddec159e0f9e7bfc0c340b2c
SvayOff <marsiul2001@gmail.com>
2022-05-21 16:04:48 +0300

    added reviews section

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,13 @@ $(function () {
     nextArrow: '<button type = "button" class= "slick-next" ><span class="icon-chevron-right"> </span> </button>',
     prevArrow: '<button type = "button" class= "slick-prev" ><span class="icon-chevron-left"> </span> </button>',
   });
+  $('.reviews__slider').slick({
+    autoplay: true,
+    autoplayInfinite: 3000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<button type = "button" class= "slick-next" ><span class="icon-chevron-right"> </span> </button>',
+    prevArrow: '<button type = "button" class= "slick-prev" ><span class="icon-chevron-left"> </span> </button>',
+  });
 });
